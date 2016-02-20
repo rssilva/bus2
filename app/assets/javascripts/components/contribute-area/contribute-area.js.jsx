4af5cb743a63b3bef6d636547c25b9428e84dd03
Rodrigo Lungui <lungui.rodrigo@gmail.com>
2016-02-20 19:02:04 -0200

    Added bus icon to contribute area

--- app/assets/javascripts/components/contribute-area/contribute-area.js.jsx
+++ app/assets/javascripts/components/contribute-area/contribute-area.js.jsx
@@ -7,6 +7,9 @@ BUS2.ContributeArea = React.createClass({
     return (
       <div className="contribute-area">
         <button className="contribute-button" onClick={this.onClick} >
+          <svg className="contribute-bus">
+            <use xlinkHref="#contribute-bus"></use>
+          </svg>
           <span className="label">Contribute</span>
         </button>
       </div>
