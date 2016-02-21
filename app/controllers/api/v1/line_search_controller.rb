2a20d4d7fb75361542f074949b2839ba39e872fd
Iuri Andreazza <iuri.andreazza@gmail.com>
2016-02-21 20:03:34 -0300

    - Adjusting Search Lines to receive null as values

--- app/controllers/api/v1/line_search_controller.rb
+++ app/controllers/api/v1/line_search_controller.rb
@@ -8,16 +8,21 @@ module Api
       #GET /api/v1/line_search/search/:country/:state/:city?:name=&[radius=10km]
       def search_lines
         queryES = {}
-        if !permited_params[:city].nil? and permited_params[:state].nil? and permited_params[:country].nil?
+        if !permited_params[:city].nil? or permited_params[:state].nil? or permited_params[:country].nil?
           queryES[:query] = {
               :bool => {
-                  :must => [
-                      {:match => { "city.name" => {:query => permited_params[:city].capitalize, :operator => 'and'}}},
-                      {:match => { "state.uf" => {:query => permited_params[:state].capitalize, :operator => 'and'}}},
-                      {:match => { "country.name" => {:query => permited_params[:country].capitalize, :operator => 'and'}}}
-                  ]
+                  :must => []
               }
           }
+          if !permited_params[:city].nil? and permited_params[:city] != "null"
+            queryES[:query][:bool][:must] << {:match => { "city.name" => {:query => permited_params[:city].capitalize, :operator => 'and'}}}
+          end
+          if !permited_params[:state].nil? and permited_params[:state] != "null"
+            queryES[:query][:bool][:must] << {:match => { "state.uf" => {:query => permited_params[:state].capitalize, :operator => 'and'}}}
+          end
+          if !permited_params[:country].nil? and permited_params[:country] != "null"
+            queryES[:query][:bool][:must] << {:match => { "country.name" => {:query => permited_params[:country].capitalize, :operator => 'and'}}}
+          end
         end
         if !permited_params[:lat].nil? and !permited_params[:lon].nil?
           queryES[:filter] = {
