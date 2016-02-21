8cb1d035fff68d8f6ab0d258212925cbfa3b9152
Rafael Specht da Silva <rafaelspdasilva@gmail.com>
2016-02-21 01:15:39 -0300

    adding contribute initial form

--- app/assets/javascripts/app.js.jsx
+++ app/assets/javascripts/app.js.jsx
@@ -3,21 +3,14 @@
 
   ReactDOM.render(<BUS2.ContributeArea />, $('.contribute-area-container')[0]);
   ReactDOM.render(<BUS2.Header />, $('.bus2-header')[0]);
-
-  var showAuthorInfo = function () {
-    console.log("showAuthorInfo");
-  };
-
-  var listBooks = function () {
-    console.log("listBooks");
-  };
+  ReactDOM.render(<BUS2.ContributeStartForm />, $('.contribute-start-form-container')[0]);
 
   var routes = {
     '/contribute': function () {
 
     },
     '/contribute/set': function () {
-
+      BUS2.Eventer.trigger('setContribution');
     }
   };
 
--- app/assets/javascripts/components/contribute-area/contribute-area.js.jsx
+++ app/assets/javascripts/components/contribute-area/contribute-area.js.jsx
@@ -1,27 +1,54 @@
-BUS2.ContributeArea = React.createClass({
-  getInitialState: function () {
-    return {
-      isOpen: false
-    }
-  },
+(function () {
+  var Eventer = BUS2.Eventer;
 
-  onClick: function () {
-    // window.history.pushState({}, '', '#/contribute/set');
-    window.location.hash = '#/contribute/set'
-  },
+  BUS2.ContributeArea = React.createClass({
+    getInitialState: function () {
+      this.setEvents();
 
-  render: function () {
-    return (
-      <div className="contribute-area">
-        <div className="contribute-button" onClick={this.onClick} >
-          <button className="contribute-button" onClick={this.onClick} >
-            <svg className="contribute-bus">
-              <use xlinkHref="#contribute-bus"></use>
-            </svg>
-            <span className="label">Contribute</span>
-          </button>
+      return {
+        isOpen: false
+      }
+    },
+
+    onClick: function (ev) {
+      var isOpen = this.state.isOpen;
+
+      if (isOpen) {
+        this.setState({isOpen: false})
+        window.location.hash = '#/';
+        Eventer.trigger('contributeClick');
+      }
+
+      if (!isOpen) {
+        this.setState({isOpen: true})
+        window.location.hash = '#/contribute/set';
+        Eventer.trigger('contributeClick');
+      }
+
+      ev.stopPropagation();
+    },
+
+    setEvents: function () {
+      var that = this;
+
+      Eventer.on('setContribution', function () {
+        that.setState({isOpen: true});
+      });
+    },
+
+    render: function () {
+      return (
+        <div className="contribute-area">
+          <div className="contribute-button" onClick={this.onClick} >
+            <button className="contribute-button" onClick={this.onClick} >
+              <svg className="contribute-bus">
+                <use xlinkHref="#contribute-bus"></use>
+              </svg>
+              <span className="label">Contribute</span>
+            </button>
+          </div>
         </div>
-      </div>
-    )
-  }
-});
+      )
+    }
+  });
+})();
