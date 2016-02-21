6d12bab8691b939dc853145ad25ccd0ed7697cc6
Pedro Cavalheiro <pedrohc@gmail.com>
2016-02-21 18:14:37 -0300

    removing unused endpoints

--- spec/requests/alerts_spec.rb
+++ spec/requests/alerts_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Alerts", type: :request do
+  fixtures :all
   describe "GET /alerts" do
     it "works! (now write some real specs)" do
 
-      ScoresController.any_instance.stubs(:current_user).returns(User.new(id: users(:one).id))
-      
+      AlertsController.any_instance.stubs(:current_user).returns(User.new(id: users(:one).id))
+
       get alerts_path
 
       expect(response).to have_http_status(200)
