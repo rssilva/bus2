76a3bf88bea30c549a10f223986c8432a660f4ed
Rodrigo Lungui <lungui.rodrigo@gmail.com>
2016-02-21 10:35:53 -0300

    Adjustments on searching view

--- app/assets/javascripts/components/search/search-view.js.jsx
+++ app/assets/javascripts/components/search/search-view.js.jsx
@@ -1,23 +1,51 @@
 BUS2.SearchView = React.createClass({
+
+  searchHandler: function(event) {
+    event.preventDefault();
+
+    var inputCountry = ReactDOM.findDOMNode($('#country')[0]),
+        inputState = ReactDOM.findDOMNode($('#state')[0]),
+        inputCity = ReactDOM.findDOMNode($('#city')[0]);
+
+
+
+  },
+
+  shareGPS: function() {
+    if (navigator.geolocation) {
+
+      navigator.geolocation.getCurrentPosition(function(position){
+        console.log(position);
+      });
+
+    }
+  },
+
   render: function () {
     return (
       <div className="bus2-search-wrapper">
-        <h2>Encontre seu ônibus em tempo real</h2>
-        <form action="">
-          <BUS2.SearchInput className="search-input" id="country" placeholder="Selecione um país" />
-          <BUS2.SearchInput className="search-input" id="state" placeholder="Selecione um estado" />
-          <BUS2.SearchInput className="search-input" id="city" placeholder="Selecione uma cidade" />
+        <div className="bus2-searching">
+          <span>BUSCANDO...</span>
+        </div>
 
-          <a className="share-gps">Compartilhe sua localização</a>
+        <div className="bus2-search-content">
+          <h2>Encontre seu ônibus em tempo real</h2>
+          <form action="" onSubmit={this.searchHandler}>
+            <BUS2.SearchInput className="search-input" id="country" placeholder="Selecione um país" name="country" />
+            <BUS2.SearchInput className="search-input" id="state" placeholder="Selecione um estado" name="state" />
+            <BUS2.SearchInput className="search-input" id="city" placeholder="Selecione uma cidade" name="city" />
 
-          <BUS2.SearchInput className="search-input" id="line" placeholder="SELECIONE UMA LINHA" />
+            <a className="share-gps" onClick={this.shareGPS}>Compartilhe sua localização</a>
 
-          <div className="bus-line-wrapper">
-            <BUS2.SearchBusLineSelect busLineNumber="343" busLineName="Ipiranga Puc" />
-          </div>
+            <BUS2.SearchInput className="search-input" id="line" placeholder="SELECIONE UMA LINHA" name="line" />
 
-          <input type="submit" className="search-go" value="Buscar" />
-        </form>
+            <div className="bus-line-wrapper">
+              <BUS2.SearchBusLineSelect busLineNumber="343" busLineName="Ipiranga Puc" />
+            </div>
+
+            <input type="submit" className="search-go" value="Buscar" />
+          </form>
+        </div>
       </div>
     )
   }
