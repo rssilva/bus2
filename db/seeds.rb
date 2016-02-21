5801ac175a066e91a9dad933e6e8a384a3de5380
Iuri Andreazza <iuri.andreazza@gmail.com>
2016-02-21 18:36:17 -0300

    - Adjustments to seed to create records without the carriage return string

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,7 +43,7 @@ Dir["tmp/csv_cidades_ibge/*.csv"].each{ |file_name|
     data.split("\n").each{ |cityName|
       puts "     - city: "+cityName
       city = City.find_or_create_by({
-                                            :name => cityName,
+                                            :name => cityName.tr("\n","").tr("\r",""),
                                             :state_id => state.id
                                         })
     }
